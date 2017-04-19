86aef8ac4938674d496fd320ab4322de1fa04807
Abhiram <abhi.fsi01@gmail.com>
2017-04-19 23:20:55 +0530

    synonyms,antonys,example,word of the day functions added

--- definition.js
+++ definition.js
@@ -9,20 +9,70 @@ var wn = new Wordnik({
 });
 
 console.log("hello")
-var questions = [
+var question1 = [
    {
-    type: 'input',
-    name: 'word',
-    message: 'Defination of ',
+    type: 'list',
+    name: 'type',
+    message: 'select :',  
+    choices: ['Defination', 'Synonyms', 'Antonyms','Examples','Word of the Day Full Dict','Full Dict'],
   }]
 
-inquirer.prompt(questions).then(function (answer) {
-  console.log(answer)
-  wn.definitions(answer.word, function(e, defs) {
-        // console.log(e, defs);
+inquirer.prompt(question1).then(function (answer1) {
+  console.log(question1)
+  console.log(answer1)
+  var question2 = [
+    {
+      type:'input',
+      name:'word',
+      message:answer1.type,
+    }]
+
+  if(answer1.type=='Defination' || answer1.type=='Full Dict'){
+    inquirer.prompt(question2).then(function (answer2) {
+      wn.definitions(answer2.word, function(e, defs) {
         for(var i=0;i<defs.length;i++){
           console.log("meaning:",i+1,":")
           console.log(defs[i].text)
         }
-  })
+      })
+    })
+  };
+  if(answer1.type=='Synonyms'|| answer1.type=='Full Dict'){
+    inquirer.prompt(question2).then(function (answer2) {
+      wn.related(answer2.word, function(e, defs) {
+        for(var i=0;i<defs.length;i++){
+          console.log("meaning:",i+1,":")
+          console.log(defs[i])
+        }
+      })
+    })
+  };
+  if(answer1.type=='Antonyms'|| answer1.type=='Full Dict'){
+      inquirer.prompt(question2).then(function (answer2) {
+        wn.related(answer2.word, function(e, defs) {
+          for(var i=0;i<defs.length;i++){
+            console.log("meaning:",i+1,":")
+            console.log(defs[i])
+          }
+        })
+      })
+    };
+  if(answer1.type=='Examples'|| answer1.type=='Full Dict'){
+      inquirer.prompt(question2).then(function (answer2) {
+        wn.examples(answer2.word, function(e, defs) {
+          for(var i=0;i<defs.examples.length;i++){
+            console.log("meaning:",i+1,":")
+            console.log(defs.examples[i])
+          }
+        })
+      })
+    };
+  if(answer1.type=='Word of the Day Full Dict'){
+      // inquirer.prompt(question2).then(function (answer2) {
+        wn.wordOfTheDay( function(e, defs) {
+          console.log(defs)
+        })
+      // })
+    };
 });
+
