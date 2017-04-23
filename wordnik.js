170faa1109621b46463f78c8eeba40bdeb4ffcb1
Abhiram <abhi.fsi01@gmail.com>
2017-04-23 18:14:20 +0530

    def,examples,synonyms,antonyms,full dict done

--- wordnik.js
+++ wordnik.js
@@ -8,7 +8,6 @@ var wn = new Wordnik({
     api_key: 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
 });
 
-console.log("hello")
 var question1 = [
    {
     type: 'list',
@@ -18,8 +17,8 @@ var question1 = [
   }]
 
 inquirer.prompt(question1).then(function (answer1) {
-  console.log(question1)
-  console.log(answer1)
+  // console.log(question1)
+  // console.log(answer1)
   var question2 = [
     {
       type:'input',
@@ -41,8 +40,11 @@ inquirer.prompt(question1).then(function (answer1) {
     inquirer.prompt(question2).then(function (answer2) {
       wn.related(answer2.word, function(e, defs) {
         for(var i=0;i<defs.length;i++){
-          console.log("meaning:",i+1,":")
-          console.log(defs[i])
+          if(defs[i].relationshipType=="synonym")
+            for(var j=0;j<defs[i].words.length;j++){
+              console.log("Synonyms:",j+1,":")
+              console.log(defs[i].words[j])
+            }
         }
       })
     })
@@ -51,9 +53,12 @@ inquirer.prompt(question1).then(function (answer1) {
       inquirer.prompt(question2).then(function (answer2) {
         wn.related(answer2.word, function(e, defs) {
           for(var i=0;i<defs.length;i++){
-            console.log("meaning:",i+1,":")
-            console.log(defs[i])
-          }
+          if(defs[i].relationshipType=="Antonym")
+            for(var j=0;j<defs[i].words.length;j++){
+              console.log("Antonyms:",j+1,":")              
+              console.log(defs[i].words[j])
+            }
+        }
         })
       })
     };
@@ -61,18 +66,21 @@ inquirer.prompt(question1).then(function (answer1) {
       inquirer.prompt(question2).then(function (answer2) {
         wn.examples(answer2.word, function(e, defs) {
           for(var i=0;i<defs.examples.length;i++){
-            console.log("meaning:",i+1,":")
-            console.log(defs.examples[i])
+            console.log("Example:",i+1,":")
+            console.log("Title",defs.examples[i].title)
+            console.log("Text",defs.examples[i].text)
           }
         })
       })
     };
   if(answer1.type=='Word of the Day Full Dict'){
-      // inquirer.prompt(question2).then(function (answer2) {
-        wn.wordOfTheDay( function(e, defs) {
-          console.log(defs)
+        wn.wordOfTheDay( function(e, def) {
+          console.log("word",def.word)
+          console.log("definitions",def.definitions)
+          console.log("examples",def.examples)
+          console.log("synonms",def.synonms)
+          console.log("antonms",def.antonms)
         })
-      // })
     };
 });
 
